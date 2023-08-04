ad9b8d87ad470ca9f8cc07133c125511ed09ff73
Fernando May <fmay130@gmail.com>
2023-08-03 21:56:35 -0600

    Allow CORS added

--- lib/repository.dart
+++ lib/repository.dart
@@ -5,10 +5,20 @@ import 'package:livproducts/domain.dart';
 class ProductRepository {
   Future<List<Record>> getProducts(String searchTerm,
       {String sortOption = 'Relevancia|0'}) async {
-    final url =
-        'https://shopappst.liverpool.com.mx/appclienteservices/services/v6/plp/sf?page-number=1&search-string=$searchTerm&sort-option=$sortOption&force-plp=false&number-of-items-per-page=40&cleanProductName=false';
+    const baseUrl = 'shopappst.liverpool.com.mx';
 
-    final response = await http.get(Uri.parse(url));
+    final response = await http.get(
+      Uri.https(baseUrl,
+          '/appclienteservices/services/v6/plp/sf?page-number=1&search-string=$searchTerm&sort-option=$sortOption&force-plp=false&number-of-items-per-page=40&cleanProductName=false'),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "*",
+        "Access-Control-Allow-Credentials": "true",
+        "Content-Type": "application/json",
+        "Accept": "*/*",
+      },
+    );
     if (response.statusCode == 200) {
       final jsonMap = json.decode(response.body);
       final productsJson = jsonMap['plpResults']['records'];
